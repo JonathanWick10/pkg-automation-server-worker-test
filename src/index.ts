94f048a94dec0f4ccf620ea9609830da84375aa4
JonathanWick10 <kzmojonathan@gmail.com>
2025-09-12 16:06:16 -0500

    New package automation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-export * from './lib/piece-manager/development/pieces-builder'
+// src/index.ts
+import { piecesBuilder } from './lib/piece-manager/development/pieces-builder'
+import { flowWorker } from './lib/flow-worker'
+
+// Exporta nombrados (útil si algún consumidor también es CJS/TS)
+export { piecesBuilder, flowWorker }
+
+// Exporta default (clave para consumo ESM de un paquete CJS)
+export default { piecesBuilder, flowWorker }
+
+// El resto de re-exports que no dependen de default:
 export * from './lib/runner/engine-runner-types'
 export * from './lib/executors/flow-job-executor'
 export * from './lib/utils/webhook-utils'
-export * from './lib/flow-worker'
